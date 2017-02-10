b7cebdc5718fbbc4458d3e7e9f16c952dd8dea01
Riccardo Crippa <therickys93@gmail.com>
2017-02-10 10:12:31 +0100

    refactoring

--- src/main/java/it/itjustworks/yourbot/server/IndexResource.java
+++ src/main/java/it/itjustworks/yourbot/server/IndexResource.java
@@ -1,5 +1,10 @@
 package it.itjustworks.yourbot.server;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import org.restlet.data.MediaType;
 import org.restlet.representation.StringRepresentation;
 import org.restlet.resource.Get;
 import org.restlet.resource.ServerResource;
@@ -7,7 +12,12 @@ import org.restlet.resource.ServerResource;
 public class IndexResource extends ServerResource{
 
 	@Get
-	public StringRepresentation hello() {
-		return new StringRepresentation("It Just Works!!!");
+	public StringRepresentation hello() throws FileNotFoundException {
+		Scanner scanner = new Scanner(new File("src/main/resources/index.html"));
+		String htmlFile = scanner.useDelimiter("\\Z").next();
+		scanner.close();
+		StringRepresentation response = new StringRepresentation(htmlFile);
+		response.setMediaType(MediaType.TEXT_HTML);
+		return response;
 	}
 }
